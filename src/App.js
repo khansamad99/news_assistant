8e6a447f138423ccb639ed7718602232e676ac7a
Samad Khan <samadk619@gmail.com>
2021-04-29 01:35:49 +0530

    Add more functionalities

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React,{useState,useEffect} from 'react'
 import allanBtn from '@alan-ai/alan-sdk-web'
-import NewsCards from './components/NewsCards'
-
+import NewsCards from './components/News/NewsCards'
+import useStyles from './style'
+import { Grid ,Grow, Typography} from '@material-ui/core';
 
 const allanKey = '9ad17421040645669d468a70e2ddc29e2e956eca572e1d8b807a3e2338fdd0dc/stage'
 
 const App = () => {
   const [articles,setnewsArticles] = useState([])
-
+  const classes = useStyles()
+  
   useEffect(() => {
     allanBtn({
       key:allanKey,
@@ -19,12 +21,15 @@ const App = () => {
     })
   }, [])
 
-  console.log(articles)
+  
   return (
-    <div>
+    <div className="container">
+        <Typography variant="h2" align="center">
+          NEWS ASSISTANT
+        </Typography>
       <NewsCards articles={articles}/>
     </div>
-  )
+  );
 }
 
 export default App
